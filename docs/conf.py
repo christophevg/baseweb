0c7f4b30005034b64824d6d33ac4653b29487b08
Christophe VG <contact@christophe.vg>
2022-11-26 09:45:25 +0100

    fixes #13 failing document generation

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ release = __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "recommonmark"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
